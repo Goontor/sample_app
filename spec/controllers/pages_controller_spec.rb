2e88ae627b068307884b716132bd144d441e701e
Goontor <claverie_pierre@hotmail.fr>
2013-04-10 23:39:17 +0200

    final update

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -7,6 +7,10 @@ describe PagesController do
       get 'home'
       response.should be_success
     end
+    it 'devrais avoir un titre'do
+      get 'home'
+      response.should have_selector("title", :content => "Simple App du Tutoriel Ruby on Rails | Accueil")
+    end
   end
 
   describe "GET 'contact'" do
@@ -14,6 +18,19 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+    it 'devrais avoir un titre'do
+      get 'contact'
+      response.should have_selector("title", :content => "Simple App du Tutoriel Ruby on Rails | Contact")
+    end
   end
-
+  describe "GET 'about'" do
+    it "devrait reussir" do
+      get 'about'
+      response.should be_success
+    end
+    it 'devrais avoir un titre'do
+      get 'about'
+      response.should have_selector("title", :content => "Simple App du Tutoriel Ruby on Rails | A Propos")
+    end
+  end
 end
